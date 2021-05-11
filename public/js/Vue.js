65a9cf63db8a7b4a95c57667aef6d3619a665bbc
Pechenyice <german.iskhakov@mail.ru>
2021-05-11 03:07:47 +0300

    backend ready

--- public/js/Vue.js
+++ public/js/Vue.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     themes[0].addEventListener('click', () => {
         if (themes[0].classList.contains('theme__option_active')) return;
+        localStorage.setItem('theme', 0);
         document.getElementsByClassName('theme__activator')[0].style.transform = 'translateX(0px)';
         themes[1].classList.remove('theme__option_active');
         themes[0].classList.add('theme__option_active');
@@ -19,6 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     themes[1].addEventListener('click', () => {
         if (themes[1].classList.contains('theme__option_active')) return;
+        localStorage.setItem('theme', 1);
         document.getElementsByClassName('theme__activator')[0].style.transform = 'translateX(73px)';
         themes[0].classList.remove('theme__option_active');
         themes[1].classList.add('theme__option_active');
@@ -28,14 +30,44 @@ window.addEventListener('DOMContentLoaded', () => {
         root.style.setProperty('--content-color', "rgb(48, 48, 48)");
     });
 
+    let theme = localStorage.getItem('theme');
+    
+    if (theme == 0) themes[0].click();
+    if (theme == 1) themes[1].click();
+
     let info = new Vue({
         el: '#info',
         data: {
-            age: 30,
-            weight: 100,
-            height: 100,
+            age: 0,
+            weight: 0,
+            height: 0,
             sex: 'man'
-        }   
+        },
+        methods: {
+            update: async function() {
+                let url = `/api/updateUser`
+                let resp = await fetch(url, {
+                    method: 'POST',
+                    headers: new Headers({
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json'
+                    }),
+                    mode: 'same-origin',
+                    body: JSON.stringify({
+                        age: this.age,
+                        weight: this.weight,
+                        height: this.height,
+                        sex: this.sex
+                    })
+                });
+        
+                try {
+                    resp = await resp.json();
+        
+                    if (resp.status == 'error') alert(resp.message);
+                } catch {}
+            }
+        }
     });
 
     let addMeal = new Vue({
@@ -53,8 +85,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
                 ingrs.meals[type].push({name: this.newName, cal: +this.newCal});
 
+                this.registerMeal();
+
                 this.newCal = null;
                 this.newName = "";
+
             },
             setType: function(index, typer) {
                 let cookers = document.getElementById('addMeal').getElementsByClassName('cooking__cook');
@@ -65,6 +100,28 @@ window.addEventListener('DOMContentLoaded', () => {
 
                 type = cookers[index].dataset.type;
                 cookers[index].classList.add('cooking__cook_active');
+            },
+            registerMeal: async function() {
+                let url = `/api/registerMeal`
+                let resp = await fetch(url, {
+                    method: 'POST',
+                    headers: new Headers({
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json'
+                    }),
+                    mode: 'same-origin',
+                    body: JSON.stringify({
+                        type: type,
+                        name: this.newName,
+                        cal: this.newCal
+                    })
+                });
+        
+                try {
+                    resp = await resp.json();
+        
+                    if (resp.status == 'error') alert(resp.message);
+                } catch {}
             }
         }
     });
@@ -113,6 +170,70 @@ window.addEventListener('DOMContentLoaded', () => {
                 return res;
             }
         }
+    });
+
+    let me = null;
+
+    (async function() {
+        let url = `/api/getMe`
+        let resp = await fetch(url, {
+            method: 'GET',
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }),
+            mode: 'same-origin'
+        });
+
+        resp = await resp.json();
+
+        if (resp.status == 'error') alert(resp.message);
+
+        document.getElementsByClassName('profile__name')[0].innerHTML = `${resp.name} ${resp.surname}`;
+        info.age = resp.age;
+        info.weight = resp.weight;
+        info.height = resp.height;
+        info.sex = resp.sex;
+    })();
+
+    (async function() {
+        let url = `/api/getMeals`
+        let resp = await fetch(url, {
+            method: 'GET',
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }),
+            mode: 'same-origin'
+        });
+
+        resp = await resp.json();
+
+        if (resp.status == 'error') alert(resp.message);
+
+        for (let e of resp.data) {
+            ingrs.meals[e.type].push({name: e.name, cal: e.cal});
+        }
+    })();
+
+    document.getElementsByClassName('profile__action')[0].addEventListener('click', async () => {
+        let url = `/api/exit`
+        let resp = await fetch(url, {
+            method: 'POST',
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }),
+            mode: 'same-origin'
+        });
+
+        try {
+            resp = await resp.json();
+
+            if (resp.status == 'error') alert(resp.message);
+        } catch {}
+
+        location.replace('/auth');
     });
 
 });
--- public/js/auth.js
+++ public/js/auth.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     themes[0].addEventListener('click', () => {
         if (themes[0].classList.contains('theme__option_active')) return;
+        localStorage.setItem('theme', 0);
         document.getElementsByClassName('theme__activator')[0].style.transform = 'translateX(0px)';
         themes[1].classList.remove('theme__option_active');
         themes[0].classList.add('theme__option_active');
@@ -17,6 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     themes[1].addEventListener('click', () => {
         if (themes[1].classList.contains('theme__option_active')) return;
+        localStorage.setItem('theme', 1);
         document.getElementsByClassName('theme__activator')[0].style.transform = 'translateX(73px)';
         themes[0].classList.remove('theme__option_active');
         themes[1].classList.add('theme__option_active');
@@ -24,6 +26,95 @@ window.addEventListener('DOMContentLoaded', () => {
         root.style.setProperty('--background-accent-color', "rgb(231, 221, 74)");
         root.style.setProperty('--accent-color', "rgb(201, 187, 0)");
         root.style.setProperty('--content-color', "rgb(48, 48, 48)");
+    });
+
+    let theme = localStorage.getItem('theme');
+    
+    if (theme == 0) themes[0].click();
+    if (theme == 1) themes[1].click();
+
+    let login = new Vue({
+        el: '#login',
+        data: {
+            login: "",
+            password: ""
+        },
+        methods: {
+            log: async function() {
+
+                if (!this.login || !this.password) {
+                    alert('Fill all the fields!');
+                    return;
+                }
+
+                let url = '/api/logUser'
+                let resp = await fetch(url, {
+                    method: 'POST',
+                    headers: new Headers({
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json'
+                    }),
+                    mode: 'same-origin',
+                    body: JSON.stringify({
+                        login: this.login,
+                        password: this.password
+                    })
+                });
+
+                resp = await resp.json();
+
+                if (resp.status == 'error') alert(resp.message);
+                if (resp.status == 'success') location.assign('/');
+            }
+        }
+    });
+
+    let signup = new Vue({
+        el: '#signup',
+        data: {
+            login: "",
+            password: "",
+            name: "",
+            surname: "",
+            height: "",
+            weight: "",
+            age: "",
+            sex: "man"
+        },
+        methods: {
+            sign: async function() {
+
+                if (!this.login || !this.password || !this.name || !this.surname || !this.height || !this.weight || !this.age || !this.sex) {
+                    alert('Fill all the fields!');
+                    return;
+                }
+
+                let url = '/api/createUser'
+                let resp = await fetch(url, {
+                    method: 'POST',
+                    headers: new Headers({
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json'
+                    }),
+                    mode: 'same-origin',
+                    body: JSON.stringify({
+                        login: this.login,
+                        password: this.password,
+                        name: this.name,
+                        surname: this.surname,
+                        height: this.height,
+                        weight: this.weight,
+                        age: this.age,
+                        sex: this.sex,
+                    })
+                });
+
+                resp = await resp.json();
+
+                if (resp.status == 'error') alert(resp.message);
+                if (resp.status == 'success') location.assign('/');
+            }
+        }
     });
 
 });
